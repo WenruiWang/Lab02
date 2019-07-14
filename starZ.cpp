d46c8619e659c86c471bae42b4999964053a030c
Wenrui Wang <wenrui_wang@cs.ucsb.edu>
2019-07-13 22:46:16 -0700

    revise9

--- starZ.cpp
+++ starZ.cpp
@@ -2,6 +2,29 @@
 #include<cstdlib>
 #include<string>
 using namespace std;
+void assertEquals(string expected, string actual, string message);
+string starZ(int width);
+void runTests(void);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 string starZ(int width){
 	if(width<3){
@@ -22,12 +45,74 @@ string starZ(int width){
 		result+="*";
 		result+="\n";
 	} 
+
 	while(s<1){for(int f=0;f<width;f++){
 		result+="*";
 	}
 	s+=1;
 	}   result+="\n";
 	return result;}
+
+
+void runTests(void) {
+
+  // The following line works because in C and C++ when string literals
+  // are separated only by whitespace (space, tab, newline), they 
+  // automatically get concatenated into a single string literal
+
+  string starZ3Expected = 
+    "***\n"
+    " * \n"
+    "***\n";
+  
+  assertEquals(starZ3Expected,starZ(3),"starZ(3)");
+
+  string starZ4Expected = 
+    "****\n"
+    "  * \n"
+    " *  \n"
+    "****\n";
+  
+  assertEquals(starZ4Expected,starZ(4),"starZ(4)");
+
+  assertEquals("",starZ(0),"starZ(0)");
+  assertEquals("",starZ(2),"starZ(2)");
+}
+
+// Test harness
+
+void assertEquals(string expected, string actual, string message="") {
+  if (expected==actual) {
+    cout << "PASSED: " << message << endl;;
+  } else {
+    cout << "   FAILED: " << message << endl << "   Expected:[\n" << expected << "] actual = [\n" << actual << "]\n" << endl;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	int main(int argc,char*argv[]){
 		if(argc!=2){
